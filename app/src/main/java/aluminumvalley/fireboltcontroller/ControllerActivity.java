246d3d977b4e8ddc79395a380e1708bae4c59188
Hanson Ng <ng.hanson@hotmail.com>
2016-10-30 19:15:33 -0500

    Initial Bluetooth Search for devices and sending a test string to the device

--- app/src/main/java/aluminumvalley/fireboltcontroller/ControllerActivity.java
+++ app/src/main/java/aluminumvalley/fireboltcontroller/ControllerActivity.java
@@ -1,16 +1,34 @@
 package aluminumvalley.fireboltcontroller;
 
-import android.content.Intent;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.UUID;
+
 
 public class ControllerActivity extends AppCompatActivity {
 
+    private boolean deviceConnected;
     private String deviceName;
+    private BluetoothDevice device;
+
+    private BluetoothSocket mmSocket;
+    private OutputStream mmOutputStream;
+    private InputStream mmInputStream;
+
+    private Button forwardButton;
+    private Button backwardButton;
+
     private TextView deviceDisplay;
     private SeekBar powerControl = null;
 
@@ -19,13 +37,29 @@ public class ControllerActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_controller);
 
-        Intent intent = getIntent();
-        deviceName = intent.getStringExtra("BTID");
+        deviceConnected = false;
+
+        device =  getIntent().getExtras().getParcelable("BTDevice");
 
         deviceDisplay = (TextView) findViewById(R.id.currentSelectedDevice);
+        forwardButton = (Button) findViewById(R.id.forward_button);
+        forwardButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                if(deviceConnected){
+                    String msg = "TESTING";
+                    msg += "\n";
+                    try {
+                        mmOutputStream.write(msg.getBytes());
+                    }catch(Exception e){
+                        Log.e("ERROR IS: ", e.toString());
+                    }
+                }
+            }
+        });
+        backwardButton = (Button) findViewById(R.id.backwards_button);
 
         powerControl = (SeekBar) findViewById(R.id.power_control_bar);
-
         powerControl.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             int progressChanged = 0;
 
@@ -51,8 +85,23 @@ public class ControllerActivity extends AppCompatActivity {
     protected void onResume(){
         super.onResume();
 
+        deviceConnected = connectWithBTDevice();
         deviceDisplay.setText(deviceName);
 
+    }
 
+    private boolean connectWithBTDevice(){
+        //TODO CHECK THIS ID
+        try {
+            UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb"); // Standard SerialPortService ID;
+            mmSocket = device.createRfcommSocketToServiceRecord(uuid);
+            mmSocket.connect();
+            mmOutputStream = mmSocket.getOutputStream();
+            mmInputStream = mmSocket.getInputStream();
+            return true;
+        }catch(Exception e){
+            Log.e("ERROR IS: ", e.toString());
+        }
+        return false;
     }
 }
--- app/src/main/java/aluminumvalley/fireboltcontroller/MainActivity.java
+++ app/src/main/java/aluminumvalley/fireboltcontroller/MainActivity.java
@@ -1,10 +1,11 @@
 package aluminumvalley.fireboltcontroller;
 
 import android.app.ProgressDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
-import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.util.TypedValue;
 import android.view.View;
@@ -16,6 +17,7 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,15 +30,19 @@ public class MainActivity extends AppCompatActivity {
     TextView message;
     TextView deviceName;
 
+    BluetoothDevice selectedDevice;
     String selectedDeviceName;
 
     ProgressDialog mProgress;
 
     ListView deviceList;
+    Set<BluetoothDevice> pairedDevices;
     List<String> foundDevices;
 
     Button selectorButton;
     Button startBTSearchButton;
+
+    BluetoothAdapter mBluetoothAdapter;
 
 
     @Override
@@ -44,27 +50,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        timerTest = (TextView)findViewById(R.id.testTimer);
-        message = (TextView)findViewById(R.id.message);
+        timerTest = (TextView) findViewById(R.id.testTimer);
+        message = (TextView) findViewById(R.id.message);
 
-        Typeface customFont = Typeface.createFromAsset(getAssets(),  "fonts/LobsterTwo-Regular.ttf");
+        Typeface customFont = Typeface.createFromAsset(getAssets(), "fonts/LobsterTwo-Regular.ttf");
         message.setTypeface(customFont);
 
         //Takes selected device and goes to the next activity
-        selectorButton = (Button)findViewById(R.id.selector_button);
+        selectorButton = (Button) findViewById(R.id.selector_button);
         selectorButton.setOnClickListener(new View.OnClickListener() {
                                               @Override
                                               public void onClick(View v) {
-                                                  Intent i = new Intent(MainActivity.this, ControllerActivity.class);
-                                                  i.putExtra("BTID", selectedDeviceName);
-                                                  startActivity(i);
+
+                                                  if(selectedDevice != null) {
+
+                                                      //CONNECT TO DEVICE
+
+                                                      Intent i = new Intent(MainActivity.this, ControllerActivity.class);
+                                                      i.putExtra("BTDevice", selectedDevice);
+                                                      startActivity(i);
+                                                  }
                                               }
 
                                           }
         );
 
         //Start the bluetooth search
-        startBTSearchButton = (Button)findViewById(R.id.start_bt_button);
+        startBTSearchButton = (Button) findViewById(R.id.start_bt_button);
         startBTSearchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -73,69 +85,72 @@ public class MainActivity extends AppCompatActivity {
                 mProgress.setMessage("Searching for Bluetooth Devices...");
                 mProgress.show();
 
-                //Temp timer to simulate BT search
-                newTimer();
+                mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+                if (mBluetoothAdapter == null) {
+                    // Device does not support Bluetooth
+                } else if (!mBluetoothAdapter.isEnabled()) {
+                    Intent enableBT = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                    startActivityForResult(enableBT, REQUEST_ENABLE_BT);
 
+                    //SET A TIMER?? Display on test timer
+                    searchForConnections();
+                }
             }
         });
 
-            deviceList=(ListView)findViewById(R.id.device_list);
+        deviceList = (ListView) findViewById(R.id.device_list);
 
-            deviceName=(TextView) findViewById(R.id.device_name);
-            //For highlight and keeping track of the device when device selected in the list
-            //HIGHLIGHT DOESNT REMAIN: FIX
-            deviceList.setSelector(R.drawable.device_selector);
-            deviceList.setOnItemClickListener(new AdapterView.OnItemClickListener()
-            {
+        deviceName = (TextView) findViewById(R.id.device_name);
+        //For highlight and keeping track of the device when device selected in the list
+        //HIGHLIGHT DOESNT REMAIN: FIX
+        deviceList.setSelector(R.drawable.device_selector);
+        deviceList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
-                @Override
-                public void onItemClick (AdapterView < ? > parent, View view,int position,
-                long arg3){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position,
+                                                                      long arg3) {
 
                 view.setSelected(true);
                 selectedDeviceName = (String) parent.getItemAtPosition(position);
                 deviceName.setText("Device: " + selectedDeviceName);
 
+                //IS THERE A BETTER WAY??
+                for (BluetoothDevice device : pairedDevices) {
+                    if(device.getName().equals(selectedDeviceName)) {
+                        selectedDevice = device;
+                        break;
+                    }
                 }
             }
-
-            );
-
-        /*  IGNORE THIS FOR NOW
-        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        if (mBluetoothAdapter == null) {
-            // Device does not support Bluetooth
-        }else if (!mBluetoothAdapter.isEnabled()) {
-            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
         }
-        */
+
+        );
 
     }
 
-        @Override
-    public void onResume(){
+    @Override
+    public void onResume() {
         super.onResume();
     }
 
     /**
      * After BT connections are found, display all the found connections
      */
-    private void finishedSearching(){
-        mProgress.dismiss();
+    private void searchForConnections() {
+
+        foundDevices = new ArrayList<String>();
+        pairedDevices = mBluetoothAdapter.getBondedDevices();
+        if (pairedDevices.size() > 0) {
+            for (BluetoothDevice device : pairedDevices) {
+                foundDevices.add(device.getName());
+            }
+        }
+
         message.setText("Availible Connections");
-        message.setTextSize(TypedValue.COMPLEX_UNIT_PX,getResources().getDimension(R.dimen.secondary_title));
+        message.setTextSize(TypedValue.COMPLEX_UNIT_PX, getResources().getDimension(R.dimen.secondary_title));
         startBTSearchButton.setVisibility(View.INVISIBLE);
         selectorButton.setVisibility(View.VISIBLE);
 
-        foundDevices = new ArrayList<String>();
-
-        //------------TEMP--------------------//
-        foundDevices.add("Device 1: 00000000");
-        foundDevices.add("Device 2: 00000000");
-        foundDevices.add("Device 3: 00000000");
-        foundDevices.add("Device 4: 00000000");
-        //------------------------------------//
 
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
                 this,
@@ -144,22 +159,9 @@ public class MainActivity extends AppCompatActivity {
                 foundDevices);
 
         deviceList.setAdapter(arrayAdapter);
+
+        mProgress.dismiss();
         deviceList.setVisibility(View.VISIBLE);
     }
 
-    private void newTimer(){
-
-        new CountDownTimer(4000, 1000) {
-
-            public void onTick(long millisUntilFinished) {
-                timerTest.setText("seconds remaining: " + millisUntilFinished / 1000);
-            }
-
-            public void onFinish() {
-                timerTest.setText("Done!");
-                finishedSearching();
-            }
-        }.start();
-
-    }
 }

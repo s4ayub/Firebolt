32557a94acbbec471bb0b1c9042bb66819675cd9
Arnet Tran <arnettran@gmail.com>
2017-01-19 21:30:28 -0500

    bluetooth connection check using timer interrupts

--- Arduino_Code/Failsafe/failsafe/failsafe.ino
+++ Arduino_Code/Failsafe/failsafe/failsafe.ino
@@ -1,3 +1,5 @@
+#include <SoftwareSerial.h>
+
 //Pin Mappings
 #define BLUETOOTH_RX                  0
 #define BLUETOOTH_TX                  1
@@ -8,6 +10,38 @@
 //Initialization of Bluetooth Module
 SoftwareSerial bluetooth(BLUETOOTH_RX, BLUETOOTH_TX);
 
+//Global variables
+bool bluetoothConnectionFlag = 0;
+String messageFromBluetooth;
+
+void checkBluetoothConnection() {
+  bluetooth.print("AT");
+  if (bluetooth.available()) {
+    char charFromBluetooth;
+    while (bluetooth.available()) {
+    delay(10);
+    messageFromBluetooth += charFromBluetooth;
+    }
+  }
+  if (messageFromBluetooth != "OK") {
+    Serial.print("Turn down motor");
+  }
+  messageFromBluetooth = "";
+}
+
+void interruptSetup() {
+  noInterrupts();
+  TCCR2B = 0;
+  TCCR2B |= CS20;
+  TIMSK2 |= TOIE;
+  interrupts();
+}
+
+//Possible alternatives: TIM2_OVF, Timer2_OVF_vect
+void ISR(TIMER2_OVF) {
+  bluetoothConnectionFlag ^= true;
+}
+
 void setup() {
   //Initalize Serial Motor for Debugging Purposes
   Serial.begin(9600);
@@ -17,6 +51,5 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  bluetoothConnectionFlag ? checkBluetoothConnection :
 }
